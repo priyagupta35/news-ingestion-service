caeddccd36c07c37e3f1b72553f05d0f745d3e19
Priya Gupta <priyaguptaindia56@gmail.com>
2026-09-13 00:07:04 +0530

    fixed the CategoryRepository.java  file

--- src/main/java/com/techpulse/ingestion/repository/CategoryRepository.java
+++ src/main/java/com/techpulse/ingestion/repository/CategoryRepository.java
@@ -1,21 +1,9 @@
 package com.techpulse.ingestion.repository;
 
-
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techpulse.ingestion.model.Article;
 import com.techpulse.ingestion.model.Category;
-import com.techpulse.ingestion.model.Source;
 
-public interface CategoryRepository extends JpaRepository<Category,Integer>
-    {
-
-    Optional<Article> findById(Long categoryId);
-
-    Optional<Source> findByName(String string);
-
-    }
-
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Optional<Category> findByName(String name);
+}
