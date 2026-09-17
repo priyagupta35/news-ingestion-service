e872a9fe995f853fd3ac164beb328a95203c2ba6
Priya Gupta <priyaguptaindia56@gmail.com>
2026-09-18 00:34:03 +0530

    update

--- src/main/java/com/techpulse/ingestion/controller/IngestionController.java
+++ src/main/java/com/techpulse/ingestion/controller/IngestionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techpulse.ingestion.repository.ArticleRepository;
 import com.techpulse.ingestion.service.NewsIngestionService;
 
 @RestController
